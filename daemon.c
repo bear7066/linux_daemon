64c58aca2220d8de5e634cf8b5fadaf70dd33024
Harry <kidflash7011@gmail.com>
2024-02-14 23:08:19 +0800

    add explaination

--- daemon.c
+++ daemon.c
@@ -4,11 +4,12 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+#include <signal.h>
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARCS 1
 
 
-int main(int argc, char** argv){
+int main(int argc, char* argv[]){
 
     char *basePath = NULL;
     char *token = NULL;
@@ -18,8 +19,22 @@ int main(int argc, char** argv){
     }
 
     basePath = (char *)malloc(sizeof(char) * sizeof(argv[1]) + 1);
+    // path = basePath 
     strcpy(basePath, argv[1]);
+    // 印出原始路徑：printf("%s", basePath);
     token = strtok(basePath, "/");
+    
+    while(token != NULL){
+        //printf("%s\n", token);
+        basePath = token;
+        token = strtok(NULL, "/");
+
+    }
+    // printf("%s\n", basePath);
+   /*
+    while(true){
+
+    }*/
 
     // a failed program result usually return non-zero value 
     exit(EXT_SUCCESS);
