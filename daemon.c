f501e179c59a07f7cb2990c0d95cf5144fe0c83d
Harry <kidflash7011@gmail.com>
2024-02-14 08:49:10 +0800

    first commit

--- daemon.c
+++ daemon.c
@@ -3,16 +3,23 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARCS 1
 
 
 int main(int argc, char** argv){
 
+    char *basePath = NULL;
+    char *token = NULL;
     if(argc < 2){
         fprintf(stderr, "Usage: we need more arcs\n");
         exit(EXT_ERR_TOO_FEW_ARCS);
     }
+
+    basePath = (char *)malloc(sizeof(char) * sizeof(argv[1]) + 1);
+    strcpy(basePath, argv[1]);
+    token = strtok(basePath, "/");
 
     // a failed program result usually return non-zero value 
     exit(EXT_SUCCESS);
