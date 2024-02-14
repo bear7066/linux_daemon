d11f4caabcf7dbc39ae7b67ab60bf792a823218b
Your Name <you@.com>
2024-02-14 08:01:48 -0800

    inotify

--- daemon.c
+++ daemon.c
@@ -6,14 +6,18 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/inotify.h>
+
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARCS 1
+#define EXT_ERR_INIT_INOTIFY 2
 
+int IeventQueue = -1;
 
 int main(int argc, char* argv[]){
 
     char *basePath = NULL;
     char *token = NULL;
+        
     if(argc < 2){
         fprintf(stderr, "Usage: we need more arcs\n");
         exit(EXT_ERR_TOO_FEW_ARCS);
@@ -34,10 +38,12 @@ int main(int argc, char* argv[]){
     // printf("%s\n", basePath);
     // blocking operation => let program run until it stuck by itself
     // blocking operation => program hand on code and wait for some event to continue
-    while(true){
-
+    IeventQueue = inotify_init();
+    if(IeventQueue == -1){
+	fprintf(stderr, "Error initializing inotify instance!\n");
+	exit(EXT_ERR_INIT_INOTIFY);
     }
-
+    
     // a failed program result usually return non-zero value 
     exit(EXT_SUCCESS);
 }
