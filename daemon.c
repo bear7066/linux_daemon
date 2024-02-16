02df81ed199b195246a05d874d192efa2eaaf378
Your Name <you@example.com>
2024-02-16 14:43:03 +0800

    buffer

--- daemon.c
+++ daemon.c
@@ -12,14 +12,22 @@
 #define EXT_ERR_TOO_FEW_ARCS 1
 #define EXT_ERR_INIT_INOTIFY 2
 #define EXT_ERR_ADD_WATCH 3
+#define EXT_ERR_BASE_PATH_NULL 4
+#define EXT_ERR_READ_INOTIFY 5
+#define EXT_ERR_INIT_LIBNOTIFY 6 
 
 int IeventQueue = -1;
 int IeventStatus = -1;
 
 int main(int argc, char* argv[]) {
+
     char *basePath = NULL;
     char *token = NULL;
-    const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF;
+    char buffer[4096];
+	// return the number of characters
+	int readLength = 0;
+
+	const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: we need more arcs\n");
@@ -35,7 +43,10 @@ int main(int argc, char* argv[]) {
         basePath = token;
         token = strtok(NULL, "/");
     }
-    
+	if(basePath == NULL){
+		fprintf(stderr, "Error getting base path!");
+		exit(EXT_ERR_BASE_PATH_NULL);
+	}
     // blocking operation => let program run until it stuck by itself
     // blocking operation => program hand on code and wait for some event to continue
     IeventQueue = inotify_init();
@@ -49,6 +60,14 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Error adding file to watch instance!\n");
         exit(EXT_ERR_ADD_WATCH);
     }
+	
+	while(true){
+		printf("Waiting for ievent...\n");
+		readLength = read(IeventQueue, buffer, sizeof(buffer));
+	
+		for(char *bufferPointer){
 
-    exit(EXT_SUCCESS);
+		}
+	}
+    // exit(EXT_SUCCESS);
 }
