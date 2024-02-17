9c4d678c40c8d488fc44e425be7381a71de2189b
Harry <86918643+bear7066@users.noreply.github.com>
2024-02-18 00:13:15 +0800

    indention

--- daemon.c
+++ daemon.c
@@ -7,51 +7,47 @@
 #include <signal.h>
 #include <sys/inotify.h>
 
-// a failed program result usually return non-zero value 
-#define EXT_SUCCESS 0
-#define EXT_ERR_TOO_FEW_ARCS 1
-#define EXT_ERR_INIT_INOTIFY 2
-#define EXT_ERR_ADD_WATCH 3
+// A failed program result usually returns a non-zero value 
+#define EXT_SUCCESS           0
+#define EXT_ERR_TOO_FEW_ARCS  1
+#define EXT_ERR_INIT_INOTIFY  2
+#define EXT_ERR_ADD_WATCH     3
 #define EXT_ERR_BASE_PATH_NULL 4
-#define EXT_ERR_READ_INOTIFY 5
-#define EXT_ERR_INIT_LIBNOTIFY 6 
+#define EXT_ERR_READ_INOTIFY  5
 
 int IeventQueue = -1;
 int IeventStatus = -1;
 
 int main(int argc, char* argv[]) {
-
     char *basePath = NULL;
     char *token = NULL;
     char *notificationMessage = NULL;
-    
     char buffer[4096];
-	// return the number of characters
     int readLength = 0;
-
     const struct inotify_event* watchEvent;
     const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF;
 
     if (argc < 2) {
-        fprintf(stderr, "Usage: we need more arcs\n");
+        fprintf(stderr, "Usage: we need more args\n");
         exit(EXT_ERR_TOO_FEW_ARCS);
     }
 
     basePath = (char *)malloc(sizeof(char) * strlen(argv[1]) + 1);
     strcpy(basePath, argv[1]);
-    // print the original path：printf("%s", basePath);
+    // Print the original path: printf("%s", basePath);
     token = strtok(basePath, "/");
     
     while (token != NULL) {
         basePath = token;
         token = strtok(NULL, "/");
     }
-    if(basePath == NULL){
-	fprintf(stderr, "Error getting base path!");
-	exit(EXT_ERR_BASE_PATH_NULL);
+    if (basePath == NULL) {
+        fprintf(stderr, "Error getting base path!\n");
+        exit(EXT_ERR_BASE_PATH_NULL);
     }
-    // blocking operation => let program run until it stuck by itself
-    // blocking operation => program hand on code and wait for some event to continue
+    
+    // Blocking operation => let program run until it's stuck by itself
+    // Blocking operation => program waits for some event to continue
     IeventQueue = inotify_init();
     if (IeventQueue == -1) {
         fprintf(stderr, "Error initializing inotify instance!\n");
@@ -64,23 +60,23 @@ int main(int argc, char* argv[]) {
         exit(EXT_ERR_ADD_WATCH);
     }
 	
-    while(true){
-	printf("Waiting for ievent...\n");
-	readLength = read(IeventQueue, buffer, sizeof(buffer));
-	if(readLength = -1){
-		fprintf(stderr, "Error reading from inotify instance!\n");
-		exit(EXT_ERR_READ_INOTIFY);
-	}
-	for(char *bufferPointer = buffer; bufferPointer < buffer + readLength;
-			bufferPointer += sizeof(struct inotify_event) + watchEvent->len){
-		watchEvent = (const struct inotify_event *) bufferPointer;
-		if(watchEvent->mask & IN_CREATE){
-			notificationMessage = "File is created.";
-		}
-		if(watchEvent->mask & IN_DELETE){
-			notificationMessage = "File is deleted.";
-		}
-	}
-	}
+    while (true) {
+        printf("Waiting for event...\n");
+        readLength = read(IeventQueue, buffer, sizeof(buffer));
+        if (readLength == -1) {
+            fprintf(stderr, "Error reading from inotify instance!\n");
+            exit(EXT_ERR_READ_INOTIFY);
+        }
+        for (char *bufferPointer = buffer; bufferPointer < buffer + readLength;
+                bufferPointer += sizeof(struct inotify_event) + watchEvent->len) {
+            watchEvent = (const struct inotify_event *) bufferPointer;
+            if (watchEvent->mask & IN_CREATE) {
+                notificationMessage = "File is created.";
+            }
+            if (watchEvent->mask & IN_DELETE) {
+                notificationMessage = "File is deleted.";
+            }
+        }
+    }
     // exit(EXT_SUCCESS);
 }
